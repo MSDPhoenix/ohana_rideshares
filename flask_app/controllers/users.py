2c35b053af5c66d624f7cd9074a9729d03fbf5cf
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-08-25 20:17:04 +0800

    completed users.py in controllers

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,8 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect,request,session,flash
 from flask_app.models.user import User
-from flask_app.models.ride import Ride
-from flask_app.models.message import message
+# from flask_app.models.ride import Ride
+# from flask_app.models.message import message
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt(app)
@@ -14,4 +14,35 @@ def index():
 @app.route("/register/",methods=["POST"])
 def register():
     if not User.validate(request.form):
-        session["xxx"] = request.form["xxx"]
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        session["email"] = request.form["email"]
+        session["password"] = request.form["password"]
+        session["confirm_password"] = request.form["confirm_password"]
+        return redirect("/")
+    pw_hash = bcrypt.generate_password_hash(request.form["password"])
+    data = {
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"],
+        "password" : pw_hash
+    }
+    user_id = User.save(data)
+    session["user_id"] = user_id
+    return redirect("/dashboard/")
+
+
+@app.route("/login/",methods=["POST"])
+def login():
+    user = User.get_by_email(request.form)
+    if not user or not bcrypt.check_password_hash(user.password,request.form["password"]):
+        flash("Invalid email/password","login")
+        return redirect("/")
+    session.clear()
+    session["user_id"] = user.id
+    return redirect("/dashboard/")
+
+@app.route("/logout/")
+def logout():
+    session.clear()
+    return redirect("/")
